9e955124d1de2a14bb206eb6e7b72cf48c011136
baozhuona <baozhuona@bytedance.com>
2020-12-28 14:46:48 +0800

    feat: highlight

--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -1,14 +1,6 @@
 const { getProcessor } = require("./libs/bytemd.min.js");
 const gfm = require("./libs/gfm.min.js");
 const highlight = require("./libs/highlight.min.js");
-const processor = getProcessor({
-  plugins: [
-    gfm(),
-    highlight({
-      subset: ["javascript", "html", "css"]
-    })
-  ]
-});
 
 Component({
   properties: {
@@ -16,64 +8,41 @@ Component({
       type: String,
       value: '',
       observer: function (newval, oldval) {
-        console.log('✨newval: ', newval);
         this.getMDNodes(newval)
       },
     }, 
-    pnode: null,
-    code: null
+		highlight: {
+			type: Boolean,
+			value: false
+		},
+    _pnode: null, // Used for recursive rendering
+    _code: null // Used for recursive rendering
   },
   data: {
-    // 组件内部数据
     nodes: {},
-  },
-  lifetimes: {
-    created: function () {
-      this.app = getApp();
-    },
-    ready: function () { }
   },
   methods: {
     getMDNodes(md) {
+      const processor = getProcessor({
+        plugins: [
+          gfm(),
+          this.data.highlight && highlight({
+            subset: ["javascript", "html", "css"]
+          })
+        ]
+      });
       const ast = processor.runSync(processor.parse(md));
-      ast.children = ast.children.filter((item) => !(item.type === 'text' && item.value.trim() === ''))
-      this.parseTreeJson(ast.children, ast);
-      console.log('ast: ', ast);
+      ast.children = this.recursiveTree(ast.children, ast);
       this.setData({ nodes: ast });
     },
-    parseTreeJson(array, parentNode) {
-      for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        if (element.children && typeof element.children == "object") {
-          if (element.tagName === "pre") {
-            element.codeIndex = this.data.codeIndex++;
-          }
-          this.parseTreeJson(element.children, element);
-        } else {
-          // 子节点 && text
-          if (element.type == "text") {
-            let text = element.value;
-            if (parentNode.tagName !== "code" && parentNode.tagName !== "span") {
-              // 换行全部干掉
-              text = text.replace(/(\r?\n)+|(\r)+/g, "");
-            } else {
-              element.value = text;
-            }
-          }
+    recursiveTree(array, parentNode) {
+      array = array.filter((item) => {
+        if (item.children && typeof Array.isArray(item.children)) {
+          item.children = this.recursiveTree(item.children, item);
         }
-      }
-    },
-    onLinkClick(event) {
-      // tt.openSchema({
-      //   schema: event.currentTarget.dataset.url,
-      //   external: true,
-      //   success(res) {
-      //     console.log(`${res}`);
-      //   },
-      //   fail(res) {
-      //     console.log(`open fail`);
-      //   }
-      // });
+        return !(item.type === 'text' && item.value.trim() === '' && parentNode.tagName !== "code" && parentNode.tagName !== "span")
+      })
+      return array
     },
     imagePreview({
       currentTarget: {
--- tools/demo/example/markdown/demo.md.js
+++ tools/demo/example/markdown/demo.md.js
@@ -6,13 +6,13 @@ module.exports = `
 ##### h5 Heading
 ###### h6 Heading
 
-**bold text**.<br>
+**bold text**
 *Italic text*
 ~~Delete line~~
 > Quoted text
 ***
 
-无序列表
+Unordered list
 - First item
 - Second item
 - Third item
@@ -20,7 +20,7 @@ module.exports = `
     - Indented item
 - Fourth item
 
-有序列表
+Ordered list
 1. First item
 2. Second item
 3. Third item
@@ -28,11 +28,11 @@ module.exports = `
     2. Indented item
 4. Fourth item
 
-图片
-![小狗狗](https://avatars2.githubusercontent.com/u/26648209?v=4&s=400&u=4108ba0a850f9f390a449b40629651a8e31d05cd '我喜欢的狗狗')
+image
+![juejin](https://sf6-scmcdn2-tos.pstatp.com/xitu_juejin_web/img/logo.a7995ad.svg 'I like juejin')
 
-链接
-[baidu](https://www.baidu.com "我是百度")
+[a link](/example/emoji/emoji "Link in WeChat Mini Program")
+[一个链接](/example/emoji/emoji "小程序内链接")
 
 \`\`\`html
 <html>
@@ -51,11 +51,11 @@ for(var i = 0; i< 11; i++) {
 const home = i18n.home;
 \`\`\`
 
-这是一段普通的文字，中间有一点\`代码\`。
+Inline code\`code\`。
 
-换行</br>
+br</br>
 
-一段长数字: 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+A long number 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
 
 | Tables        | Are           | Cool  |
 | ------------- |:-------------:| -----:|
@@ -63,10 +63,4 @@ const home = i18n.home;
 | col 2 is      | centered      |   $12 |
 | zebra stripes | are neat      |    $1 |
 
-<video>
-<source src="http://html5demos.com/assets/dizzy.mp4">
-</source>
-</video>
-
-<video src="http://html5demos.com/assets/dizzy.mp4"></video>
 `;
